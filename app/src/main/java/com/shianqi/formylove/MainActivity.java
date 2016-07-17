3ae1629dbb837e28a408db4904c12b87cb9b9f4b
shianqi <shianqi@imudges.com>
2016-07-18 00:22:43 +0800

    线程调用

--- app/src/main/java/com/shianqi/formylove/MainActivity.java
+++ app/src/main/java/com/shianqi/formylove/MainActivity.java
@@ -1,9 +1,7 @@
 package com.shianqi.formylove;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,8 +11,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Handler handler = new Handler(){
+        public void handleMessage(android.os.Message msg){
+            switch (msg.what){
+                case 1:
+                    handler.sendEmptyMessageDelayed(1,100);
+                    break;
+                default: break;
+            }
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        handler.sendEmptyMessage(1);
     }
 
     @Override
