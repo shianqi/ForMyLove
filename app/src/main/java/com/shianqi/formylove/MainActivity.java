867903629f0ce0c033327cf5089833003cf13339
shianqi <shianqi@imudges.com>
2016-07-18 23:49:33 +0800

    秒表功能完成

--- app/src/main/java/com/shianqi/formylove/MainActivity.java
+++ app/src/main/java/com/shianqi/formylove/MainActivity.java
@@ -8,18 +8,36 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private TextView timeDay;
+    private TextView timeHour;
+    private TextView timeMinute;
+    private TextView timeSecond;
+    private TextView timeAll;
+
+    private int intDay;
+    private int intHour;
+    private int intMinute;
+    private int intSecond;
+    private int intSecond1;
+    private int intTimeAll;
 
     private Handler handler = new Handler(){
         public void handleMessage(android.os.Message msg){
             switch (msg.what){
                 case 1:
-                    handler.sendEmptyMessageDelayed(1,100);
+                    setTime();
+                    handler.sendEmptyMessageDelayed(1,200);
                     break;
                 default: break;
             }
@@ -53,7 +71,45 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        timeDay = (TextView)findViewById(R.id.day);
+        timeHour = (TextView)findViewById(R.id.hour);
+        timeMinute = (TextView)findViewById(R.id.mint);
+        timeSecond = (TextView)findViewById(R.id.sec);
+        timeAll = (TextView)findViewById(R.id.secs);
+
+
+        //timeAll.setText(System.currentTimeMillis()+"");
+
+
+        //Log.i("时间",calendar.getTimeInMillis()+"");
+        setTime();
         handler.sendEmptyMessage(1);
+    }
+
+    private void setTime(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR,2016);
+        calendar.set(Calendar.MONTH,5);
+        calendar.set(Calendar.DAY_OF_MONTH,16);
+        calendar.set(Calendar.HOUR_OF_DAY, 22);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+
+        Long nowTime = System.currentTimeMillis();
+
+        intDay = (int)((nowTime-calendar.getTimeInMillis())/(1000 * 60 * 60 * 24));
+        intHour = (int)((nowTime-calendar.getTimeInMillis())/(1000 * 60 * 60)-24*intDay);
+        intMinute = (int)((nowTime-calendar.getTimeInMillis())/(1000 * 60 )-24*intDay*60-intHour*60);
+        intSecond = (int)((nowTime-calendar.getTimeInMillis())/(1000)
+                -24*intDay*60*60-intHour*60*60-intMinute*60);
+
+        intTimeAll = (int)((nowTime-calendar.getTimeInMillis())/(1000));
+
+        timeDay.setText(intDay+"");
+        timeHour.setText(intHour+"");
+        timeMinute.setText(intMinute+"");
+        timeSecond.setText(intSecond+"");
+        timeAll.setText(intTimeAll+"秒");
     }
 
     @Override
